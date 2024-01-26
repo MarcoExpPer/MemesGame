c15ab01e325a3b61877a3717ba81caaf7ba6d720
Marco Exp <marcoexp@ucm.es>
2024-01-26 21:09:29 +0100

    Added UE5 project

---
+++ Source/MemesGames/MemesGamesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MemesGamesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEMESGAMES_API AMemesGamesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
